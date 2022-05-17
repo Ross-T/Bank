f21a65a99d9b49ec9f3325e3b3972fb23dc6fbf0
Ross Turpin <ross_turpin@yahoo.co.uk>
2022-05-17 11:49:15 +0200

    Added withdrawal method

--- Bank.java
+++ Bank.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
+
 public class Bank {
-    
+    String name;
+    String accNo;
+    double balance;
+    double lastTransaction;
+
+    public void Account(String customerName, String accountNo) {
+        name = customerName;
+        accNo = accountNo;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Invalid request, withdrawal amount exceeds available balance.");
+        } else if (amount <= 0) {
+            System.out.println("Invalid request, withdrawal amount must equal above £0");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal of £" + amount + "successful, new balance: £" + balance);
+        }
+    }
+
+
+
+
+
+
 
     public static void main(String[] args) {
-        
-
         
     }
 }
